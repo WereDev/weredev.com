ac87bd25fa78aa6f47ef057237ef58b214c2dbba
weredev <dmaschmeier@weredev.com>
2018-11-24 19:41:04 -0600

    got initial flickr thing working

---
+++ src/worldtraveler/flickrProvider.tsx
@@ -0,0 +1,43 @@
+const flickrUrl = 'https://api.flickr.com/services/rest/?api_key=d15bbd08117fc0ecf6bc7ef1a66a0240&user_id=29735288%40N05&format=json&nojsoncallback=1&method=';
+const getCollectionsMethod = "flickr.collections.getTree";
+
+export class FlickrProvider {
+  
+  public collections : IFlickrCollection[];
+
+  public getCollections() {
+      let t = this;
+
+      return fetch(flickrUrl + getCollectionsMethod)
+      .then(response => response.json())
+      .then(json => {
+        let thing = (json as IFlickrCollectionResponse);
+        t.collections = thing.collections.collection;
+        return t.collections;
+      });
+  }
+}
+
+export interface IFlickrCollectionResponse {
+  collections: IFlickrCollections,
+  stat: string
+}
+
+export interface IFlickrCollections {
+  collection: IFlickrCollection[]
+}
+
+export interface IFlickrCollection {
+  id: string,
+  title: string,
+  description: string,
+  iconlarge: string,
+  iconsmall: string,
+  set: IFlickrSet[]
+}
+
+export interface IFlickrSet {
+  id: string,
+  title: string,
+  description: string
+}
--- src/worldtraveler/index.tsx
+++ src/worldtraveler/index.tsx
@@ -1,13 +1,39 @@
 import * as React from 'react';
+import * as Flickr from './flickrProvider';
 
-class Index extends React.Component {
+
+class Index extends React.Component<any, IndexState, any> {
+  
+  public componentWillMount() {
+    this.setState({flickrProvider: new Flickr.FlickrProvider()});
+  }
+
+  public componentDidMount() {
+    this.state.flickrProvider.getCollections()
+    .then(response => {
+      this.setState({ collections: response });
+    });
+  }
+  
+
   public render() {
+    const collections : any[] = [];
+    if (this.state.collections) {
+      this.state.collections.forEach(element => {
+            collections.push(<div>{element.title}</div>)
+      });
+    }
     return (
       <div>
-        travel photos coming soon...
+        {collections}
       </div>
     );
   }
 }
 
 export default Index;
+
+interface IndexState {
+  flickrProvider: Flickr.FlickrProvider,
+  collections: Flickr.IFlickrCollection[]
+}
