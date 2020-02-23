a8a9e4aaa9d61a45d8a1e79567d764c7ceb4e69f
WereDev <github@weredev.com>
2020-02-22 22:37:27 -0600

    removing other http requirement

--- Weredev.UI/Controllers/BaseController.cs
+++ Weredev.UI/Controllers/BaseController.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Weredev.UI.Controllers
 {
-    [RequireHttps]
     public abstract class BaseController : Controller
     {
         public void SetTitle(string title)
